6567686518cf9ceaba7d0a065e4067c82dcfeb55
rickoji <kojiurakawa@gmail.com>
2024-12-01 21:46:51 -0300

    change background based on media

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
    const img = document.querySelector('#profile img')
 
   if(html.classList.contains('light')){
-     img.setAttribute('src', './assets/avatar-light.png')
-     img.setAttribute('alt','avatar no modo light')
+     img.setAttribute("src", "./assets/avatar-light.png")
+     img.setAttribute("alt","avatar no modo light")
     }else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute("alt", "avatar no modo normal")
     }
   
